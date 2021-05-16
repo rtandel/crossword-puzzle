93d493bcbb97520913d7eb9fd5ffcf38b1c44530
rtandel <rtandel124@gmail.com>
2021-05-16 19:23:38 -0400

    Just some changes

--- crossword-react/src/components/Container.js
+++ crossword-react/src/components/Container.js
@@ -4,6 +4,16 @@ import './container.css'
 export default function Container() {
   const list = new Array(25).fill("k");
   const square = Math.sqrt(list.length) % 2 == 0 ? Math.sqrt(list.length) : 0;
+
+  function generateRows() {
+    let rowArray = [];
+    for (let i = 0; i < square; i++ ) {
+        rowArray.add(<h1>item</h1>)
+    }
+
+
+  }
+
   return (
     <div>
       <ul>{list.map((value, key) => {
